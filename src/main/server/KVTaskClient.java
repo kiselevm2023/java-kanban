48b1d256cfc41bc2d870c9ae9b152ef554c851a3
kiselevm2023 <kiselevm2023@yandex.ru>
2023-11-12 20:55:04 +0400

    3 коммит

--- src/main/server/KVTaskClient.java
+++ src/main/server/KVTaskClient.java
@@ -1,62 +1,38 @@
 package main.server;
 
 import java.io.IOException;
+import main.exceptions.ManagerSaveException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-
-import main.exceptions.ManagerSaveException;
 
 public class KVTaskClient {
 
-    private final String apiToken;
+    private String apiToken;
+
     private final String serverURL;
 
-    public KVTaskClient(String serverURL) throws IOException, InterruptedException {
+    public KVTaskClient(String serverURL) {
         this.serverURL = serverURL;
-
-        URI uri = URI.create(this.serverURL + "/register");
-
-        try {
-        HttpRequest request = HttpRequest
-                .newBuilder()
-                .GET()
-                .uri(uri)
-                .header("Content-Type", "application/json")
-                .build();
-
-            HttpClient client = HttpClient.newHttpClient();
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString()
-            );
-
-            if (response.statusCode() != 200) {
-                throw new ManagerSaveException ("An error occurred during registration");
-            }
-            apiToken = response.body();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            throw new ManagerSaveException ("Error occurred during the request");
-        }
+        register(serverURL);
+        System.out.println("API " + apiToken);
     }
 
     public void put(String key, String json) {
         URI uri = URI.create(this.serverURL + "/save/" + key + "?API_TOKEN=" + apiToken);
-
         HttpRequest request = HttpRequest
                 .newBuilder()
                 .POST(HttpRequest.BodyPublishers.ofString(json))
                 .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
                 .header("Content-Type", "application/json")
                 .build();
 
         HttpClient client = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
         try {
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8)
-            );
+            HttpResponse<String> response = client.send(request, handler);
             if (response.statusCode() != 200) {
                 throw new ManagerSaveException ("Data not saved");
             }
@@ -68,28 +44,48 @@ public class KVTaskClient {
 
     public String load(String key) {
         URI uri = URI.create(this.serverURL + "/load/" + key + "?API_TOKEN=" + apiToken);
-
-        try {
-        HttpRequest request = HttpRequest.newBuilder()
+        HttpRequest request = HttpRequest
+                .newBuilder()
                 .GET()
                 .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
                 .header("Content-Type", "application/json")
                 .build();
 
         HttpClient client = HttpClient.newHttpClient();
-
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8)
-            );
-
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        try {
+            HttpResponse<String> response = client.send(request, handler);
             if (response.statusCode() != 200) {
-                throw new RuntimeException("Error, Data is not founded");
-                //System.out.println("Data is not founded ");
+                System.out.println("Data is not founded ");
             }
             return response.body();
         } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            throw new ManagerSaveException ("Error occurred during the request");
+            System.out.println("Error occurred during the request");
+            return null;
+        }
+    }
+
+    void register(String url) {
+        URI uri = URI.create(url + "/register");
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .GET()
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Content-Type", "application/json")
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        try {
+            HttpResponse<String> response = client.send(request, handler);
+            if (response.statusCode() != 200) {
+                throw new ManagerSaveException ("An error occurred during registration");
+            }
+            this.apiToken = response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new ManagerSaveException ("Error occurred during registration");
         }
     }
 }
